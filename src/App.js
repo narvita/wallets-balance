1d912acea4db9a9418e9c0b128654c5a87a1710e
narvita <narkha21@gmail.com>
2022-09-03 12:59:06 +0400

    get user wallet balance

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Web3 from "web3/dist/web3.min.js";
+import "./App.css";
 
 function App() {
+  const [web3, setWeb3] = useState();
+  const [balance, setBalance] = useState("");
+  const [walletAddress, setWallets] = useState("");
+
+  const initWeb3 = async () => {
+    const web3 = new Web3(window.ethereum);
+    const rawBalance = await web3.eth.getBalance(walletAddress);
+    const balance = web3.utils.fromWei(rawBalance);
+    console.log(walletAddress);
+    console.log("rawBalance", rawBalance, "balance", balance);
+    setBalance(balance);
+    setWeb3(web3);
+  };
+
+  const handleGetBalance = (e) => {
+    setWallets(e.target.value);
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    initWeb3();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        id="message"
+        name="message"
+        onChange={handleGetBalance}
+        value={walletAddress}
+        autoComplete="off"
+      ></input>
+      <button onClick={handleClick}>Get Wallet Address</button>
+      <p>{balance}</p>
     </div>
   );
 }
